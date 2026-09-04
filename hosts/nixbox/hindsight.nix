16d123afa6ac351f18e766c008569cc75ec47c47
Hassan Munir <hassanmunir@live.com>
2026-09-04 12:29:25 +0100

    fix(hindsight): keep inference local

--- hosts/nixbox/hindsight.nix
+++ hosts/nixbox/hindsight.nix
@@ -17,7 +17,7 @@
           LLAMA_ARG_HF_REPO = "google/gemma-4-12B-it-qat-q4_0-gguf";
           LLAMA_ARG_HF_FILE = "gemma-4-12b-it-qat-q4_0.gguf";
           LLAMA_ARG_CTX_SIZE = "65536";
-          LLAMA_ARG_N_PARALLEL = "2";
+          LLAMA_ARG_N_PARALLEL = "1";
           LLAMA_ARG_N_GPU_LAYERS = "99";
           LLAMA_ARG_MMPROJ_AUTO = "false";
           LLAMA_ARG_REASONING = "off";
@@ -33,7 +33,7 @@
       };
 
       hindsight = {
-        image = "ghcr.io/vectorize-io/hindsight:0.9.1";
+        image = "ghcr.io/vectorize-io/hindsight:0.9.2";
         pull_policy = "always";
         restart = "unless-stopped";
         depends_on.llama.condition = "service_healthy";
@@ -42,9 +42,11 @@
           HINDSIGHT_API_LLM_BASE_URL = "http://llama:8080/v1";
           HINDSIGHT_API_LLM_API_KEY = "not-needed";
           HINDSIGHT_API_LLM_MODEL = "gemma-4-12b";
-          HINDSIGHT_API_LLM_MAX_CONCURRENT = "2";
+          HINDSIGHT_API_LLM_MAX_CONCURRENT = "1";
           HINDSIGHT_API_LLM_TIMEOUT = "300";
           HINDSIGHT_API_LLM_MAX_RETRIES = "2";
+          HINDSIGHT_API_REFLECT_MAX_ITERATIONS = "4";
+          HINDSIGHT_API_REFLECT_MAX_CONTEXT_TOKENS = "32768";
           HINDSIGHT_API_WORKER_ID = "nixbox-hindsight";
         };
         ports = [
